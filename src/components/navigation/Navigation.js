1f76374dd8ae13cf2fa2995e7c8805eeb9825e6a
bmccoy04 <bryanmccoy04@gmail.com>
2017-03-20 20:33:58 -0500

    fixed up the navigation.

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router';
 
 class Navigation extends Component {
   render() {
     return (
-      <div className='navbar navbar-default'>
-        Navi
+      <div className='navbar navbar-default navbar-fixed-top'>
+        <div className="container">
+          <ul className="nav navbar-nav">            
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/servers">Servers</Link></li>
+          </ul>
+        </div>
       </div>
     );
   }
